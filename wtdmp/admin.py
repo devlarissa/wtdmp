96783ef9093601a27aad4868d30d3a15bc3ecce5
devlarissa <devlarissasilva@gmail.com>
2022-12-22 05:44:51 -0300

    Creating model package

---
+++ wtdmp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import XD
+from .models import Package
+
+admin.site.register(XD)
+admin.site.register(Package)
--- wtdmp/models.py
+++ wtdmp/models.py
@@ -3,3 +3,13 @@ from django.db import models
 class XD(models.Model):
     lat = models.FloatField()
     lng = models.FloatField()
+
+    def __str__(self):
+        return '[' + str(self.lat) + ', ' + str(self.lng) + ']'
+
+class Package(models.Model):
+    lat = models.FloatField()
+    lng = models.FloatField()
+
+    def __str__(self):
+        return '[' + str(self.lat) + ', ' + str(self.lng) + ']'
